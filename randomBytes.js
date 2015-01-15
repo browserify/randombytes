9e99f197e01a17b47c88ae0e6eaf1a2199737c2a
Calvin Metcalf <cmetcalf@appgeo.com>
2015-01-15 09:31:04 -0500

    readme

--- randomBytes.js
+++ randomBytes.js
@@ -6,13 +6,18 @@ if(crypto.getRandomValues) {
 } else {
   module.exports = oldBrowser;
 }
-function randomBytes(size) {
+function randomBytes(size, cb) {
   var bytes = new Buffer(size); //in browserify, this is an extended Uint8Array
     /* This will not work in older browsers.
      * See https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
      */
 
   crypto.getRandomValues(bytes);
+  if (typeof cb === 'function') {
+    return process.nextTick(function () {
+      cb(null, bytes);
+    });
+  }
   return bytes;
 }
 function oldBrowser() {
--- test.js
+++ test.js
@@ -1,9 +1,21 @@
 var test = require("tape");
 var randomBytes = require('./');
 
-test('correct length', function (t) {
+test('sync', function (t) {
     t.plan(3);
-    t.equals(randomBytes(3).length, 3, 3);
-    t.equals(randomBytes(30).length, 30, 30);
-    t.equals(randomBytes(300).length, 300, 300);
+    t.equals(randomBytes(3).length, 3, 'len: ' + 3);
+    t.equals(randomBytes(30).length, 30, 'len: ' + 30);
+    t.equals(randomBytes(300).length, 300, 'len: ' + 300);
+});
+test('async', function (t) {
+    t.plan(3);
+    randomBytes(3, function (err, resp) {
+      t.equals(resp.length, 3, 'len: ' + 3);
+    });
+    randomBytes(30, function (err, resp) {
+      t.equals(resp.length, 30, 'len: ' + 30);
+    });
+    randomBytes(300, function (err, resp) {
+      t.equals(resp.length, 300, 'len: ' + 300);
+    });
 });
