4e3fac8461c06b23a639699ca9c4c9dd5f988482
Daniel Cousens <github@dcousens.com>
2015-03-27 12:53:51 +1100

    use standard.js

--- browser.js
+++ browser.js
@@ -1,28 +1,30 @@
-'use strict';
+'use strict'
+
+function oldBrowser () {
+  throw new Error('secure random number generation not supported by this browser\nuse chrome, FireFox or Internet Explorer 11')
+}
 
 var crypto = global.crypto || global.msCrypto
-if(crypto && crypto.getRandomValues) {
-  module.exports = randomBytes;
-} else {
-  module.exports = oldBrowser;
-}
-function randomBytes(size, cb) {
-  var bytes = new Buffer(size); //in browserify, this is an extended Uint8Array
-    /* This will not work in older browsers.
-     * See https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
-     */
 
-  crypto.getRandomValues(bytes);
+if (crypto && crypto.getRandomValues) {
+  module.exports = randomBytes
+} else {
+  module.exports = oldBrowser
+}
+
+function randomBytes (size, cb) {
+  // in browserify, this is becomes an extended UInt8Array
+  var bytes = new Buffer(size)
+
+  // This will not work in older browsers.
+  // See https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
+  crypto.getRandomValues(bytes)
+
   if (typeof cb === 'function') {
     return process.nextTick(function () {
-      cb(null, bytes);
-    });
+      cb(null, bytes)
+    })
   }
-  return bytes;
-}
-function oldBrowser() {
-  throw new Error(
-      'secure random number generation not supported by this browser\n'+
-      'use chrome, FireFox or Internet Explorer 11'
-    )
+
+  return bytes
 }
--- test.js
+++ test.js
@@ -1,12 +1,13 @@
-var test = require("tape");
-var randomBytes = require('./');
+var test = require('tape')
+var randomBytes = require('./')
 
 test('sync', function (t) {
-    t.plan(3);
-    t.equals(randomBytes(3).length, 3, 'len: ' + 3);
-    t.equals(randomBytes(30).length, 30, 'len: ' + 30);
-    t.equals(randomBytes(300).length, 300, 'len: ' + 300);
-});
+  t.plan(3)
+  t.equals(randomBytes(3).length, 3, 'len: ' + 3)
+  t.equals(randomBytes(30).length, 30, 'len: ' + 30)
+  t.equals(randomBytes(300).length, 300, 'len: ' + 300)
+})
+
 test('async', function (t) {
   t.plan(3)
 
@@ -31,13 +32,14 @@ test('async', function (t) {
 
 if (process.browser) {
   test('requesting to much throws', function (t) {
-    t.plan(1);
-    t.throws(randomBytes.bind(null, 65537));
-  });
+    t.plan(1)
+    t.throws(randomBytes.bind(null, 65537))
+  })
+
   test('requesting to much throws async', function (t) {
-    t.plan(1);
+    t.plan(1)
     t.throws(randomBytes.bind(null, 65537, function () {
-      t.ok(false, 'should not get here');
-    }));
-  });
+      t.ok(false, 'should not get here')
+    }))
+  })
 }
