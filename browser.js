1cdf05706bfabc3ea485c8a371062cb0cc9b4f17
Calvin Metcalf <cmetcalf@appgeo.com>
2018-07-17 14:58:35 -0400

    zero copy large buffer random bytes

--- browser.js
+++ browser.js
@@ -1,5 +1,13 @@
 'use strict'
 
+// limit of Crypto.getRandomValues()
+// https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
+var MAX_BYTES = 65536
+
+// Node supports requesting up to this number of bytes
+// https://github.com/nodejs/node/blob/master/lib/internal/crypto/random.js#L48
+var MAX_UINT32 = 4294967295
+
 function oldBrowser () {
   throw new Error('Secure random number generation is not supported by this browser.\nUse Chrome, Firefox or Internet Explorer 11')
 }
@@ -15,14 +23,21 @@ if (crypto && crypto.getRandomValues) {
 
 function randomBytes (size, cb) {
   // phantomjs needs to throw
-  if (size > 65536) throw new Error('requested too many random bytes')
+  if (size > MAX_UINT32) throw new Error('requested too many random bytes')
   // in case browserify  isn't using the Uint8Array version
   var bytes = Buffer.allocUnsafe(size)
 
   // This will not work in older browsers.
   // See https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
   if (size > 0) {  // getRandomValues fails on IE if size == 0
-    crypto.getRandomValues(bytes)
+    if (size > MAX_BYTES) {
+      for (var generated = 0; generated < size; generated += MAX_BYTES) {
+        var nextChunk = generated + MAX_BYTES > size ? size - generated : MAX_BYTES
+        crypto.getRandomValues(bytes.slice(generated, generated + nextChunk))
+      }
+    } else {
+      crypto.getRandomValues(bytes)
+    }
   }
 
   if (typeof cb === 'function') {
