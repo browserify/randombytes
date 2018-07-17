5e893058568c0ae77bb719e288d096dc63e83ee7
Calvin Metcalf <cmetcalf@appgeo.com>
2018-07-17 14:47:57 -0400

    roll back phantom js stuff since we are on safe-buffer

--- browser.js
+++ browser.js
@@ -17,16 +17,13 @@ function randomBytes (size, cb) {
   // phantomjs needs to throw
   if (size > 65536) throw new Error('requested too many random bytes')
   // in case browserify  isn't using the Uint8Array version
-  var rawBytes = new global.Uint8Array(size)
+  var bytes = Buffer.allocUnsafe(size)
 
   // This will not work in older browsers.
   // See https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
   if (size > 0) {  // getRandomValues fails on IE if size == 0
-    crypto.getRandomValues(rawBytes)
+    crypto.getRandomValues(bytes)
   }
-
-  // XXX: phantomjs doesn't like a buffer being passed here
-  var bytes = Buffer.from(rawBytes.buffer)
 
   if (typeof cb === 'function') {
     return process.nextTick(function () {
