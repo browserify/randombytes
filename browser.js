b28d4ecee46262801ea09f15fa1f1513a05c5971
RMacfarlane <ramacfar@microsoft.com>
2020-07-15 14:18:46 -0700

    Use globalThis code in place of global

--- browser.js
+++ browser.js
@@ -13,6 +13,17 @@ function oldBrowser () {
 }
 
 var Buffer = require('safe-buffer').Buffer
+
+// ref: https://github.com/tc39/proposal-global
+var getGlobal = function () {
+	if (typeof self !== 'undefined') { return self }
+	if (typeof window !== 'undefined') { return window }
+	if (typeof global !== 'undefined') { return global }
+	throw new Error('unable to locate global object')
+}
+
+var global = getGlobal()
+
 var crypto = global.crypto || global.msCrypto
 
 if (crypto && crypto.getRandomValues) {
