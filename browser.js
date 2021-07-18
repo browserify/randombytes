0e5d823893c54a18fcbdd28a3c274c7d58287d8b
Jimmy Wärting <jimmy@warting.se>
2021-07-18 16:40:44 +0200

    standard --fix

--- browser.js
+++ browser.js
@@ -2,17 +2,17 @@
 
 // limit of Crypto.getRandomValues()
 // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
-var MAX_BYTES = 65536
+const MAX_BYTES = 65536
 
 // Node supports requesting up to this number of bytes
 // https://github.com/nodejs/node/blob/master/lib/internal/crypto/random.js#L48
-var MAX_UINT32 = 4294967295
+const MAX_UINT32 = 4294967295
 
 function oldBrowser () {
   throw new Error('Secure random number generation is not supported by this browser.\nUse Chrome, Firefox or Internet Explorer 11')
 }
 
-var crypto = global.crypto || global.msCrypto
+const crypto = global.crypto || global.msCrypto
 
 if (crypto && crypto.getRandomValues) {
   module.exports = randomBytes
@@ -24,12 +24,12 @@ function randomBytes (size, cb) {
   // phantomjs needs to throw
   if (size > MAX_UINT32) throw new RangeError('requested too many random bytes')
 
-  var bytes = Buffer.allocUnsafe(size)
+  const bytes = Buffer.allocUnsafe(size)
 
-  if (size > 0) {  // getRandomValues fails on IE if size == 0
+  if (size > 0) { // getRandomValues fails on IE if size == 0
     if (size > MAX_BYTES) { // this is the max bytes crypto.getRandomValues
       // can do at once see https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
-      for (var generated = 0; generated < size; generated += MAX_BYTES) {
+      for (let generated = 0; generated < size; generated += MAX_BYTES) {
         // buffer.slice automatically checks if the end is past the end of
         // the buffer so we don't have to here
         crypto.getRandomValues(bytes.slice(generated, generated + MAX_BYTES))
